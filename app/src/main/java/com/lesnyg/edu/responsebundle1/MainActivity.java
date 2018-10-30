32446225d402b461eddbeafa7d5b425c1a17baa4
이선영 <lsy79@hanmail.net>
2018-10-30 16:13:36 +0900

    commit2

--- app/src/main/java/com/lesnyg/edu/responsebundle1/MainActivity.java
+++ app/src/main/java/com/lesnyg/edu/responsebundle1/MainActivity.java
@@ -2,11 +2,14 @@ package com.lesnyg.edu.responsebundle1;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -21,14 +24,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        EditText editText = findViewById(R.id.editText);
-        String text = editText.getText().toString();
+        //String text = null;
+        //Bundle bundle = new Bundle();
+        //bundle.putString("text", text);
+        Intent intent = new Intent(this, secondActivity.class);
+        //intent.putExtras(bundle);
+        startActivityForResult(intent, Activity.RESULT_FIRST_USER);
 
-        Bundle bundle = new Bundle();
-        bundle.putString("text",text);
-        Intent intent = new Intent(this,secondActivity.class);
-        intent.putExtras(intent);
-        startActivityForResult(intent,Activity.RESULT_FIRST_USER);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(data.hasExtra("resultValue"))
+            Toast.makeText(getApplicationContext(),data.getStringExtra("resultValue"),Toast.LENGTH_LONG).show();
+
 
     }
 }
--- app/src/main/java/com/lesnyg/edu/responsebundle1/secondActivity.java
+++ app/src/main/java/com/lesnyg/edu/responsebundle1/secondActivity.java
@@ -1,13 +1,36 @@
 package com.lesnyg.edu.responsebundle1;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ActionMode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class secondActivity extends AppCompatActivity {
+public class secondActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+
     }
+
+    @Override
+    public void finish() {
+        EditText editText = findViewById(R.id.editText);
+
+        Intent intent = new Intent();
+        Bundle bundle = new Bundle();
+        bundle.putString("resultValue",((EditText)editText).getText().toString());
+        intent.putExtras(bundle);
+        setResult(Activity.RESULT_FIRST_USER,intent);
+        super.finish();
+    }
+
+
 }
