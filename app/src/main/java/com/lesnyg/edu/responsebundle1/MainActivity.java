47d2c91b2e238929a772ba14ee889dbe9d238f1f
이선영 <lsy79@hanmail.net>
2018-10-30 14:42:32 +0900

    commit1

--- app/src/main/java/com/lesnyg/edu/responsebundle1/MainActivity.java
+++ app/src/main/java/com/lesnyg/edu/responsebundle1/MainActivity.java
@@ -1,13 +1,34 @@
 package com.lesnyg.edu.responsebundle1;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        EditText editText = findViewById(R.id.editText);
+        String text = editText.getText().toString();
+
+        Bundle bundle = new Bundle();
+        bundle.putString("text",text);
+        Intent intent = new Intent(this,secondActivity.class);
+        intent.putExtras(intent);
+        startActivityForResult(intent,Activity.RESULT_FIRST_USER);
+
     }
 }
